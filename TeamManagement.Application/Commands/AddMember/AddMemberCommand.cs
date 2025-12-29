bd89754e3d34a5029d1235dacfa1e3d5a67f8973
Mrjackabasi <mr.jack1174@gmail.com>
2025-12-30 02:19:39 +0330

    implement team member and leader commands

--- TeamManagement.Application/Commands/AddMember/AddMemberCommand.cs
+++ TeamManagement.Application/Commands/AddMember/AddMemberCommand.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using system;
+using Application.Abstraction.CQRS;
+
+
+namespace Application.Abstraction.CQRS;
+
+public sealed record AddMember(
+    Guid TeamId,
+    Guid MemberId,
+    ) : ICommand;
 
 public class Class1
 {
